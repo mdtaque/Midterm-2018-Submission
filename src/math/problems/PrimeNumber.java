61165befe040e29ca35497345c8653559ed6f197
mdtaque <rifattaque@gmail.com>
2018-09-10 23:53:14 -0400

    math completed

--- src/math/problems/PrimeNumber.java
+++ src/math/problems/PrimeNumber.java
@@ -13,6 +13,31 @@ public class PrimeNumber {
 		 *
 		 */
 
+		int counter = 0;
+		for(int i=2; i<100; i++){
+			if(i==2){
+				System.out.println(i);
+			}else{
+			    if(isPrime(i)) {
+                    counter++;
+                    System.out.println(i);
+                }
+//                }else
+//				    System.out.println("Non-Prime: " + i);
+			}
+		}
+
+		System.out.println("Total number of Prime Numbers within this range: " + counter);
+
 	}
 
+	public static boolean isPrime(int n){
+	    if(n%2==0)
+	        return false;
+	    for(int i=3; i*i<=n; i+=2){
+	        if(n%i==0)
+	            return false;
+        }
+        return true;
+    }
 }
--- src/math/problems/UnitTestingMath.java
+++ src/math/problems/UnitTestingMath.java
@@ -1,8 +1,16 @@
 package math.problems;
 
+import org.testng.Assert;
+import org.testng.AssertJUnit;
+
 public class UnitTestingMath {
     public static void main(String[] args) {
         //Apply Unit testing into each classes and methods in this package.
 
+        Assert.assertEquals(120,Factorial.factorial(5));
+        //Assert.assertEquals(Fibonacci.main(40));
+        //Assert.assertEquals(4,FindLowestDifference.main());
+        //Assert.assertEquals(9,FindMissingNumber.missingNumber());
+        //Assert.assertEquals(5,LowestNumber.main());
     }
 }
