7fd6fcd81bcd4459a3bf90d30d61eab04f825ef9
mdtaque <rifattaque@gmail.com>
2018-09-11 15:42:38 -0400

    String done

--- src/string/problems/Palindrome.java
+++ src/string/problems/Palindrome.java
@@ -7,5 +7,23 @@ public class Palindrome {
           Palindrome. So write java code to check if a given String is Palindrome or not.
          */
 
+        String word1 = "MOM";
+        checkPalindrome(word1);
+        checkPalindrome("NAA");
+
+    }
+    public static void checkPalindrome(String word){
+        String actual = word.toLowerCase();
+        String reverse = "";
+
+        for(int i= actual.length()-1; i >= 0; i--){
+            reverse = reverse + actual.charAt(i);
+        }
+        if(actual.equals(reverse)){
+            System.out.println(word + " is a palindrome");
+        }else{
+            System.out.println(word + " is not a palindrome");
+        }
+
     }
 }
--- src/string/problems/UnitTestingStringProblem.java
+++ src/string/problems/UnitTestingStringProblem.java
@@ -1,8 +1,13 @@
 package string.problems;
 
+import org.testng.Assert;
+
 public class UnitTestingStringProblem {
     public static void main(String[] args) {
         //Apply Unit Test into all the methods in this package.
 
+        Assert.assertEquals(true,Anagram.isAnagram("ASIF","SIFA"));
+        //Assert.assertEquals("biological",DetermineLargestWord.findTheLargestWord(""));
+        //Assert.assertEquals(true,Palindrome.checkPalindrome("DAD"));
     }
 }
